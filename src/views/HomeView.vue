ec7c704d1139acb9582365159d94c17c9d3b9e57
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-07 20:41:46 -0300

    breakpoints and display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
       Dolorem.</p>
     <h1 class="display-4 text-uppercase">Massive Text</h1>
     <v-btn depressed color="indigo white--text">Click Here</v-btn>
-    <v-btn flat color="red">Click Me</v-btn>
+    <v-btn text color="red">Click Me</v-btn>
 
     <v-btn depressed>
       <v-icon left>mdi-database</v-icon>
@@ -27,6 +27,12 @@
       <v-icon dark>
         mdi-heart
       </v-icon>
+
+      <v-btn large class="hidden-md-and-up">
+      <v-icon left large>mdi-database</v-icon>
+      <span>Backup</span>
+    </v-btn>
+
     </v-btn>
 
   </div>
