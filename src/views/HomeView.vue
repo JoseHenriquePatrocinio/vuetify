6078fa669492fe6df8a671ab1567da35af012d0b
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-07 17:58:15 -0300

    inicio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p class="blue white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, repellat perspiciatis
+      numquam voluptates debitis nam at iusto ipsum nihil sed omnis odit atque, velit amet. Asperiores debitis in tempora.
+      Dolorem.</p>
+    <h1 class="display-4 text-uppercase">Massive Text</h1>
+    <v-btn depressed color="indigo white--text">Click Here</v-btn>
+    <v-btn flat color="red">Click Me</v-btn>
+
+    <v-btn depressed>
+      <v-icon left>mdi-database</v-icon>
+      <span>Backup</span>
+    </v-btn>
+
+    <v-btn small>
+      <v-icon left small>mdi-database</v-icon>
+      <span>Backup</span>
+    </v-btn>
+
+    <v-btn large>
+      <v-icon left large>mdi-database</v-icon>
+      <span>Backup</span>
+    </v-btn>
+
+    <v-btn class="mx-2" fab dark small color="pink">
+      <v-icon dark>
+        mdi-heart
+      </v-icon>
+    </v-btn>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
