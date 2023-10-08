2e947853f43dcc376cef03c258c0ececad2dd4bd
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-07 23:30:31 -0300

    add routes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
+  <div class="dashboard">
     <h1>homepage</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
+  name: 'DashboardView',
 
 }
 </script>
---
+++ src/views/Team.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="team">
+      <h1>This is the team page</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default{
+    name:'TeamView'
+  }
+  </script>
