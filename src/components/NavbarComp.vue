4a68b25cdf805bb7b3d0aabf2ec7f7009a5f5b57
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-07 22:56:30 -0300

    list

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -13,9 +13,19 @@
             </v-btn>
         </v-app-bar>
 
-        <v-navigation-drawer app v-model="drawer" class="indigo">
-
+        <v-navigation-drawer app v-model="drawer" class="primary">
+            <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
+
 
     </nav>
 </template>
@@ -25,7 +35,12 @@ export default {
     name: "NavbarComp",
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'mdi-folder', text: 'My projects', route: '/projects' },
+                { icon: 'mdi-account-group', text: 'Team', route: '/team' }
+            ]
         }
     }
 }
