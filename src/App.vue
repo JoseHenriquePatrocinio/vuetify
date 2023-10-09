348e5c5f2a198213c4367cc015c29c575d50edf9
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-08 21:04:01 -0300

    footer component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <v-app>
     <NavbarComp />
+    
     <v-main class="mx-4 mb-4">
       <router-view />
     </v-main>
+    
+    <FooterComp />
   </v-app>
 </template>
 
 <script>
 import NavbarComp from '@/components/NavbarComp.vue'
+import FooterComp from '@/components/FooterComp.vue'
 export default {
   name: 'App',
   components: {
-    NavbarComp
+    NavbarComp,
+    FooterComp
   },
   data: () => ({
 
