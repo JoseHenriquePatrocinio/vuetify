b13928d276ad30d2fa9afc0357e0306e0f82b62e
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-09 23:38:27 -0300

    avatars

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -14,6 +14,16 @@
         </v-app-bar>
 
         <v-navigation-drawer app v-model="drawer" class="primary">
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/avatar-3.png">
+                    </v-avatar>
+                    <p class="white--text subheading mt-1 text-center">
+                        Chun Li
+                    </p>
+                </v-flex>
+            </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
