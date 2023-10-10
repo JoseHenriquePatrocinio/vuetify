fb546482fa7bab878166b1d55993441f43b264f7
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-09 23:27:15 -0300

    cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,16 +1,46 @@
 <template>
-    <div class="team">
-      <h1 class="text-subtitle-1 grey--text">This is the team page</h1>
+  <div class="team">
+    <h1 class="text-subtitle-1 grey--text">This is the team page</h1>
 
-      <v-container class="my-5">
-        content
-      </v-container>
+    <v-container class="my-5">
 
-    </div>
-  </template>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card text class="text-xs-center ma-3">
+            <v-responsive class="pt-4 text-center">
+              image here
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading text-center">{{ person.name }}</div>
+              <div class="grey--text text-center">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="grey">
+                <v-icon small left>mdi-message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+  </div>
+</template>
   
-  <script>
-  export default{
-    name:'TeamView'
+<script>
+export default {
+  name: 'TeamView',
+  data() {
+    return {
+      team: [
+        { name: 'The Net Ninja', role: 'Web developer' },
+        { name: 'Ryu', role: 'Graphic designer' },
+        { name: 'Chun Li', role: 'Web developer' },
+        { name: 'Gouken', role: 'Social media maverick' },
+        { name: 'Yoshi', role: 'Sales guru' }
+      ]
+    }
   }
-  </script>
+}
+</script>
