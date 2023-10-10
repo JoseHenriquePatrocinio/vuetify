2469915023b19a05815cec29b3c9738b13d10255
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-10 00:58:24 -0300

    basic form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -10,6 +10,13 @@
                 <v-card-title>
                     Add new project
                 </v-card-title>
+                <v-card-text>
+                    <v-form class="px-3">
+                        <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
+                        <v-textarea label="Information" v-model="content" prepend-icon="mdi-edit"></v-textarea>
+                        <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+                    </v-form>
+                </v-card-text>
             </v-card>
         </v-dialog>
     </div>
@@ -17,6 +24,17 @@
 
 <script>
 export default {
-    name: "PopupComp"
+    name: "PopupComp",
+    data(){
+        return{
+            title:"",
+            content:""
+        }
+    },
+    methods:{
+        submit(){
+            console.log(this.title, this.content)
+        }
+    }
 }
 </script>
