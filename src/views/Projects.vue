594750bf4991641f453b4083eda15a138d62691f
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-10 00:10:45 -0300

    expansion panels

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,16 +1,48 @@
 <template>
-  <div class="projects">
-    <h1 class="text-subtitle-1 grey--text">This is a projects page</h1>
-
+  <div>
     <v-container class="my-5">
-        content
-      </v-container>
+
+      <v-expansion-panels>
+        <v-expansion-panel v-for="project in myProject" :key="project.title">
+          <v-expansion-panel-header>
+            {{ project.title }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card>
+              <v-card-text class="px-4 grey--text">
+                <div class="font-weight-bold">due by {{ project.due }}</div>
+                <div>{{ project.content }}</div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
+
+    </v-container>
 
   </div>
 </template>
 
 <script>
-export default{
-  name:'ProjectsView'
+export default {
+  name: 'ProjectsView',
+  data() {
+    return {
+      projects: [
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Code up the dashboard', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' },
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!' }
+      ]
+    }
+  },
+  computed: {
+    myProject() {
+      return this.projects.filter(project => {
+        return project.person === 'Chun Li'
+      })
+    }
+  }
 }
 </script>
